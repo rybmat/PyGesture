77edafaceb4f4b9f6c71e21f2fa7e45c8d3a3753
Michał <mazur@dhcppc5.home>
2013-11-17 21:26:31 +0100

    Linux mouse controller added

--- Linux_mouse_controller.py
+++ Linux_mouse_controller.py
@@ -1,23 +1,13 @@
-# poniżej masz szkielet klasy odpowiedzialnej za obsługę myszki. Znajdź jakąś bibliotekę
-# i zrób obsługę myszki pod linuxem (polecam autopy - niby na wszystkie platformy, ale
-# na makach nie śmiga - coś z openGL-em poknocili i nie chce się kompilować). 
-# Nie zmieniaj nazw publicznych metod i pól, żeby w głównym programie można było
-# korzystać z tego samego kodu dla obu systemów
-# jak chcesz zrobić metodę lub pole prywatne to nazwę zacznij od dwóch podkreślników
-# np. 
-#   self.__pole 
-# albo 
-#   def __metoda
+#-*- coding: utf-8 -*-
+import autopy
 
 
 class Mouse(): 
     """a class containing attributes and methods needed to controlling mouse cursor
     """
     def __init__(self, x=100, y=100):
-        #self.maxX =    pobierz szerokość i wysokość ekranu i przypisz do tych pól  
-        #self.maxY =
+    	self.maxX, self.maxY=autopy.screen.get_size()
         
-
         if (x in range(0, self.maxX)) and (y in range(0, self.maxY)):
             self.moveTo(x, y)
         else:
@@ -44,7 +34,7 @@ class Mouse():
             self.y = y 
 
         #print self.x, self.y
-        #TODO - coś co spowoduje przesunięcie kursora na pozycję self.x, self.y
+        autopy.mouse.move(self.x,self.y)
        
 
 
@@ -52,49 +42,15 @@ class Mouse():
         """moves coursor by x step and y step"""
         self.moveTo(self.x + x, self.y + y)
 
-    
-
-    def leftButtonDown(self):
-        """makes left button of mouse pushed"""
-        #TODO
-        pass
-
-
-    def leftButtonUp(self):
-        """makes left button of mouse unpushed"""
-        #TODO
-        pass
-
-
     def leftClick(self):
-        """makes "left click" """
-        self.leftButtonDown()
-        self.leftButtonUp()
-
+	autopy.mouse.click(autopy.mouse.LEFT_BUTTON)
 
     def doubleLeftClick(self):
         self.leftClick()
         self.leftClick()
 
-    
-
-    def rightButtonDown(self):
-        """makes right button of mouse pushed"""
-        #TODO
-        pass
-
-
-    def rightButtonUp(self):
-        """makes right button of mouse unpushed"""
-        #TODO
-        pass
-
-
     def rightClick(self):
-        """makes "right click" """
-        self.rightButtonDown()
-        self.rightButtonUp()
-
+	autopy.mouse.click(autopy.mouse.RIGHT_BUTTON)
 
     def doubleRightClick(self):
         self.rightClick()
