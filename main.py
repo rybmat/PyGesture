2a76ab744964eb5d80452c5574ea53f02270d55d
Mateusz Rybarski <rybarski.mateusz@gmail.com>
2013-12-04 20:49:58 +0100

    added new training files

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ else:
 	print "OS not supported, probably Windows ;)"
 
 
-def scale_mouse_coordinates(m,hands,camera):
-	(x, y, w, h)=hands[0]
+def scale_mouse_coordinates(m, position, camera):
+	(x, y, w, h) = position 
 	k1 = m.maxX / camera.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
 	k2 = m.maxY / camera.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
 	return int(k1 * (2 * x + w) / 2), int(k2 * (2 * y + h) / 2)
-
+	
 	
 if __name__ == '__main__':
 	
-	palm_cascade = cv2.CascadeClassifier("training_data/training6/palm/cascade.xml")
-	fist_cascade = cv2.CascadeClassifier("training_data/training6/fist/cascade.xml")
+	palm_cascade = cv2.CascadeClassifier("training_data/training7/palm/cascade.xml")
+	fist_cascade = cv2.CascadeClassifier("training_data/training7/fist/cascade.xml")
 
 	print "Opening camera..."
 	camera = cv2.VideoCapture(0)
 	working = 0
-
+	pos = (500, 500, 100, 100)
 	m=Mouse()
 	
 	cv2.namedWindow("Camera")
@@ -39,29 +39,27 @@ if __name__ == '__main__':
 	if camera.isOpened():
 		while True:
 			retval, image = camera.read()
-			
+
 			if retval:
 				flipped = cv2.flip(image, orientation)
 				img = flipped [:]
 				
 				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-				palms = palm_cascade.detectMultiScale(gray, 1.7, 6)
-				fists = fist_cascade.detectMultiScale(gray, 1.7, 6)
+				palms = palm_cascade.detectMultiScale(gray, 1.1, 7)
+				#fists = fist_cascade.detectMultiScale(gray, 1.1, 7)
 
 				for (x, y, w, h) in palms:
 					print "palm"
 					cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
-					roi_gray = gray[y:y+h, x:x+w]
-					roi_color = img[y:y+h, x:x+w]
+					pos = (x, y, w, h)
 
-				for (x, y, w, h) in fists:
-					print "fist"
-					cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-					roi_gray = gray[y:y+h, x:x+w]
-					roi_color = img[y:y+h, x:x+w]
+				movex, movey = scale_mouse_coordinates(m, pos, camera)
+				m.moveTo(movex,movey)
 
-				#movex, movey = scale_mouse_coordinates(m,hands,camera)
-				#m.moveTo(movex,movey)
+				#for (x, y, w, h) in fists:
+				#	print "fist"
+				#	cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+				#
 				
 				cv2.imshow("Camera", img)
 				if not working:
